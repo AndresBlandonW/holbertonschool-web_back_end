208debdba80e06259459abc072d5e5a71c647e96
AndresBlandonW <andblandon@hotmail.com>
2022-06-23 20:58:50 -0500

    Session authentication

---
+++ 0x07-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""manage the API authentication"""
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth:
+    """Auth class"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """method to auth"""
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+
+        if path[-1] != '/':
+            path = path + '/'
+
+        for rsl in excluded_paths:
+            if rsl[-1] == '*':
+                rsl = rsl[0:-1]
+
+            if rsl in path:
+                return False
+
+        if path not in excluded_paths:
+            return True
+
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """method authorization header"""
+        if request is None or request.headers.get('Authorization') is None:
+            return None
+
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """method current user"""
+        return None
