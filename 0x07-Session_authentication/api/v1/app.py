dc96e136664fadcfba62b57e0c05c6ecf47a8687
AndresBlandonW <andblandon@hotmail.com>
2022-06-23 21:49:10 -0500

    Update - Before request

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -50,10 +50,12 @@ def brequest():
     """
     ls = ['/api/v1/status/',
           '/api/v1/unauthorized/',
-          '/api/v1/forbidden/']
+          '/api/v1/forbidden/',
+          '/api/v1/auth_session/login/']
 
     if auth is not None and auth.require_auth(request.path, ls):
-        if auth.authorization_header(request) is None:
+        if auth.authorization_header(request) is None and\
+                auth.session_cookie(request) is None:
             abort(401)
 
         if auth.current_user(request) is None:
