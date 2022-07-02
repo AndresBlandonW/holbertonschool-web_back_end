5c2d84da7927fb29782f3ebfd853134d4525cb13
AndresBlandonW <andblandon@hotmail.com>
2022-07-02 14:34:04 -0500

    create log in system

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """Basic Flask app"""
-from flask import Flask, jsonify, request
+from urllib import response
+from flask import Flask, abort, jsonify, request
+import flask
 from auth import Auth
 
 app = Flask(__name__)
@@ -22,6 +24,23 @@ def users():
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """return sessions route"""
+    email = request.form['email']
+    password = request.form['password']
+
+    req = AUTH.valid_login(email, password)
+
+    if req is False:
+        abort(401)
+    else:
+        session_id = AUTH.create_session(email)
+        res = flask.make_response()
+        res.set_cookie('session_id', session_id)
+        return jsonify({"email": email, "message": "logged in"})
 
 
 if __name__ == "__main__":
