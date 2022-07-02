0a9077bc940b2894bd5c28849b141f6b766c35c4
AndresBlandonW <andblandon@hotmail.com>
2022-07-02 13:36:12 -0500

    Update register user auth

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
-"""password module"""
+"""auth module"""
 import bcrypt
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
+
+from user import User
 
 
 def _hash_password(password: str) -> str:
@@ -8,3 +12,21 @@ def _hash_password(password: str) -> str:
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(bytes(password, 'utf-8'), salt)
     return hashed
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """register user method"""
+        try:
+            user_exists = self._db.find_user_by(email=email)
+            if user_exists is not None:
+                raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_pass = _hash_password(password)
+            return self._db.add_user(email, hashed_pass)
