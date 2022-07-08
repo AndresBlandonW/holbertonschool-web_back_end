e545bfa119c48e56b3f2053e178cba81d221c240
AndresBlandonW <andblandon@hotmail.com>
2022-07-07 23:20:04 -0500

    Get locate from request

--- 0x0A-i18n/2-app.py
+++ 0x0A-i18n/2-app.py
@@ -1,1 +1,34 @@
 #!/usr/bin/env python3
+""""Basic Flask app"""
+from flask_babel import Babel
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# Initialize Flask-Babel
+babel = Babel(app)
+
+
+class Config(object):
+    """Config class to setup Babel for English and French"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """template index"""
+    return render_template('1-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000", debug=True)
